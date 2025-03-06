07953abfc389cd2dd8f3748233e24f768b2e578b
Yu Li <liyu3519@gmail.com>
2025-03-06 16:16:33 +0000

    feat: complete part of authentication

---
+++ backend/user/src/main/java/com/example/user/model/User.java
@@ -0,0 +1,22 @@
+package com.example.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    private String username;
+    private String password;
+}
